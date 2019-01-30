bd42188cb6b742923c0d8e3e5b83715d214d4394
Fraser Wilson <blancolioni@gmail.com>
2019-01-30 19:41:19 +0100

    package body Harriet.Employment

--- src/agents/harriet-employment.adb
+++ src/agents/harriet-employment.adb
@@ -1,7 +1,9 @@
 with Ada.Containers.Doubly_Linked_Lists;
 
 with Harriet.Agents;
+with Harriet.Calendar;
 
+with Harriet.Db.Account;
 with Harriet.Db.Installation;
 with Harriet.Db.Pop;
 
@@ -46,18 +48,50 @@ package body Harriet.Employment is
       Quantity : Harriet.Quantities.Quantity_Type;
       Salary   : Harriet.Money.Price_Type)
    is
-      use Harriet.Money;
-      Installation : Harriet.Db.Installation.Installation_Type :=
+      use Harriet.Money, Harriet.Quantities;
+      Installation : constant Harriet.Db.Installation.Installation_Type :=
                        Harriet.Db.Installation.Get_Installation (Employer);
-      Pop          : Harriet.Db.Pop.Pop_Type :=
+      Pop          : constant Harriet.Db.Pop.Pop_Type :=
                        Harriet.Db.Pop.Get_Pop (Employee);
-      Employed_Pop : Harriet.Db.Pop.Pop_Type :=
+      Account      : constant Harriet.Db.Account.Account_Type :=
+                       Harriet.Db.Account.Get (Pop.Account);
+      Employed_Pop : constant Harriet.Db.Pop.Pop_Type :=
                        Harriet.Db.Pop.Get_By_Pop_Group_Installation
                          (Pop.Pop_Group, Installation.Reference);
       Cash_Per_Pop : constant Price_Type :=
-                       Price (Harriet.Agents.Cash (Pop), Pop.Size);
+                       Price (Account.Cash, Pop.Size);
+      New_Pop_Size : constant Quantity_Type := Pop.Size - Quantity;
+      New_Account  : Harriet.Db.Account_Reference;
    begin
-      null;
+      Account.Set_Cash (Account.Cash - Total (Cash_Per_Pop, Quantity));
+      Pop.Set_Size (New_Pop_Size);
+      if Employed_Pop.Has_Element then
+         New_Account := Employed_Pop.Account;
+         Employed_Pop.Set_Size (Employed_Pop.Size + Quantity);
+      else
+         New_Account :=
+           Harriet.Db.Account.Create
+             (Harriet.Db.Null_Account_Reference, Zero, Zero);
+         Harriet.Db.Pop.Create
+           (Transported_Size => 0.0,
+            Active           => True,
+            Next_Event       => Harriet.Calendar.Clock,
+            Manager          => "default-pop",
+            Account          => New_Account,
+            Capacity         => Zero,
+            Faction          => Pop.Faction,
+            World            => Installation.World,
+            World_Sector     => Installation.World_Sector,
+            Installation     => Installation.Reference,
+            Ship             => Harriet.Db.Null_Ship_Reference,
+            Pop_Group        => Pop.Pop_Group,
+            Size             => Quantity,
+            Salary           => Salary,
+            Happiness        => Pop.Happiness);
+      end if;
+
+      Harriet.Agents.Add_Cash (New_Account, Total (Cash_Per_Pop, Quantity));
+
    end Execute;
 
    ----------------------------------
