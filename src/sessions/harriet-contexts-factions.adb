28ba3bcf97270fbe479ef459fba64472fc120b0f
Fraser Wilson <blancolioni@gmail.com>
2019-02-08 14:07:57 +0100

    Added world sector owner key; added owned worlds context to factions

--- src/sessions/harriet-contexts-factions.adb
+++ src/sessions/harriet-contexts-factions.adb
@@ -1,5 +1,11 @@
 with Harriet.Factions;
 
+with Harriet.Contexts.Containers;
+with Harriet.Contexts.Worlds;
+
+with Harriet.Db.Faction;
+with Harriet.Db.World;
+
 package body Harriet.Contexts.Factions is
 
    type Faction_Context_Type is
@@ -26,6 +32,17 @@ package body Harriet.Contexts.Factions is
       return String
    is (Harriet.Factions.Name (Context.Faction));
 
+   procedure Scan_Worlds
+     (Faction : Harriet.Db.Faction_Reference;
+      Process : not null access
+        procedure (Context : Context_Type));
+
+   package Owned_World_Containers is
+     new Harriet.Contexts.Containers
+       (Container_Name    => "worlds",
+        Context_Reference => Harriet.Db.Faction_Reference,
+        Iterate_Children  => Scan_Worlds);
+
    ---------------------
    -- Faction_Context --
    ---------------------
@@ -47,9 +64,10 @@ package body Harriet.Contexts.Factions is
      (Context  : Faction_Context_Type;
       Children : in out Context_List'Class)
    is
-      pragma Unreferenced (Context);
    begin
       Children.Clear;
+      Children.Append
+        (Owned_World_Containers.Container_Context (Context.Faction));
    end Get_Child_Contexts;
 
    --------------
@@ -64,5 +82,23 @@ package body Harriet.Contexts.Factions is
    begin
       return Context.Faction /= Harriet.Db.Null_Faction_Reference;
    end Is_Valid;
+
+   -----------------
+   -- Scan_Worlds --
+   -----------------
+
+   procedure Scan_Worlds
+     (Faction : Harriet.Db.Faction_Reference;
+      Process : not null access
+        procedure (Context : Context_Type))
+   is
+   begin
+      for World of
+        Harriet.Db.World.Select_By_Owner
+          (Harriet.Db.Faction.Get (Faction).Reference)
+      loop
+         Process (Harriet.Contexts.Worlds.World_Context (World.Reference));
+      end loop;
+   end Scan_Worlds;
 
 end Harriet.Contexts.Factions;
