b1f0ad0eb244014207a00ab1f0544390484a0aef
Fraser Wilson <blancolioni@gmail.com>
2019-04-15 14:44:34 +0200

    New script restore-state.sh

--- clean-setup.sh
+++ clean-setup.sh
@@ -2,3 +2,5 @@
 ./bin/harriet-driver --create
 ./bin/harriet-driver --add-faction --account-name=root --faction-name=Republic --faction-adjective=Republican --faction-plural=Republicans --faction-color=#003153 --faction-setup-path=capital
 ./bin/harriet-driver --add-faction --faction-names-file=faction-names.config --faction-colors-file=faction-colors.config
+echo Saving initial state
+cp harriet.db.marlowe harriet.db.init.marlowe
---
+++ restore-state.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+echo Restoring state
+cp harriet.db.init.marlowe harriet.db.marlowe
